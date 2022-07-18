d70682a95c18309b012da4b119b367dacb5eb3f5
Lee Minji <gagopa9912@naver.com>
2022-07-18 21:32:56 +0900

    0718

---
+++ 1545.py
@@ -0,0 +1,5 @@
+# 거꾸로 출력해보아요
+
+num = int(input())
+for i in range(num, -1, -1):
+    print(i, end=' ')
---
+++ 2025.py
@@ -0,0 +1,7 @@
+# N줄 덧셈
+num = int(input())
+
+total = 0
+for i in range(1, num + 1):
+    total += i
+print(total)
