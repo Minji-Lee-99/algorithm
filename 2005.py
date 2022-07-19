d9fa5ef4e55c591da526762ca618821bf48d4275
Lee Minji <gagopa9912@naver.com>
2022-07-19 21:16:23 +0900

    0719

--- 2005.py
+++ 2005.py
@@ -1,15 +1,11 @@
 #파스칼의 삼각형
-
 T = int(input())
 for test_case in range(1, T + 1):
     num = int(input())
     print(f'#{test_case}')
-    for i in range(1, num+1):
-        if i <= 2: # 1~2번째줄
-            print("1 " * i)
-        else: # 3이상 
-            print("1", end=' ')
-            for j in range(i - 2):
-                print(i - 1, end=' ')
-            print("1", end=' ')
-            print("\n", end='')
+    lst0 = []
+    lst = [1]
+    for i in range(num):
+        print(' '.join(list(map(str, lst))))
+        lst0 = [0] + lst + [0]
+        lst = [(lst0[j] + lst0[j+1]) for j in range(len(lst0)-1)]
