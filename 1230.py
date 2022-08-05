aeebb3f48b9d4973915444afccf4bcb4eeb81529
LeeMJ <gagopa9912@naver.com>
2022-08-05 17:45:21 +0900

    0805

---
+++ 1230.py
@@ -0,0 +1,14 @@
+# 암호문 3
+
+T = 10
+for test_case in range(1, T + 1):
+    N = int(input())
+    pw = list(map(int, input().split()))
+    M = int(input())
+    order = input().split()
+    for i in order:
+        if i == 'l':
+            
+        elif i == 'D':
+
+        else:
