eb5631245abc594b5b6c4bc67f6eb130720fa23b
Lee Minji <gagopa9912@naver.com>
2022-08-05 23:06:27 +0900

    0805

--- 1230.py
+++ 1230.py
@@ -6,9 +6,23 @@ for test_case in range(1, T + 1):
     pw = list(map(int, input().split()))
     M = int(input())
     order = input().split()
-    for i in order:
-        if i == 'l':
-            
-        elif i == 'D':
-
-        else:
+    order = order[::-1]
+    index = 0
+    for i in range(M):
+        od = order.pop()
+        if  od == 'I':
+            x = int(order.pop())
+            temp = []
+            for j in range(int(order.pop())):
+                temp.append(order.pop())
+            pw = pw[:x] + temp + pw[x:]
+        elif od == 'D':
+            x = int(order.pop())
+            pw = pw[:x] + pw[x+4:]
+        elif od == 'A':
+            for j in range(int(order.pop())):
+                pw.append(order.pop())
+    print(f'#{test_case}', end=' ')
+    for i in range(10):
+        print(pw[i], end=' ')
+    print('\n')
