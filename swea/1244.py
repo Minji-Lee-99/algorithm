15c1a8668e9c240f9a8caf110a1b7d20128cde32
Lee Minji <gagopa9912@naver.com>
2022-08-12 00:01:11 +0900

    0812

--- swea/1244.py
+++ swea/1244.py
@@ -1,14 +1,28 @@
 # 최대 상금
 
-import sys
-sys.stdin = open("input.txt", "r")
-
-def dfs():
-
+# DFS를 통해서 모든 경우를 확인하는데, 메모이제이션을 통해서 이미 나온 경우는 계산하지 않도록 함.
+def find_max_price(number, k, memo):
+    if k == 0: # base케이스
+        return number
+    if (number, k) in memo: # 메모에 있는 경우 메모값 리턴
+        return memo[(number, k)]
+    for i in range(len(number)): # 모든 경우의 수
+        for j in range(len(number)):
+            if i != j: # i, j가 같지 않을때만 실행
+                number2 = list(number)
+                number2[i], number2[j] = number2[j], number2[i] # 자리 바꾸기
+                num = find_max_price(''.join(number2), k-1, memo) # 자리 바꾼 상태에서 다시 자리바꾸기(k번 만큼)
+                if (number, k) in memo: # 메모에 적어주기
+                    if memo[(number, k)] < num: # 이미 메모에 있다면 새로운 값이 더 큰 경우만 바꾸기
+                        memo[(number, k)] = num
+                else:
+                    memo[(number, k)] = num
+    return memo[(number, k)] # 메모에서 최종값 리턴
 
 
 T = int(input())
 for tc in range(1, T + 1):
-    num, K = input().split()
-    K = int(K)
-    num = list(num)
+    num, k = input().split()
+    k = int(k)
+    result = find_max_price(num, k, {})
+    print(f'#{tc} {result}')
--- swea/1954.py
+++ swea/1954.py
@@ -12,13 +12,14 @@ for test_case in range(1, T + 1):
     for i in range(2, N ** 2 + 1):
         while 1:
             index %= 4
+            # 숫자를 갱신하는 경우
             if 0 <= x + dx[index] <= N-1 and 0 <= y + dy[index] <= N-1 and result[x + dx[index]][y + dy[index]] == 0:
                 x = x + dx[index]
                 y = y + dy[index]
                 result[x][y] = i
                 break
-            else:
-                index += 1
+            else: # 갱신X, 인덱스를 벗어나거나 이미 바뀐 경우
+                index += 1 #방향백터 갱신
     print(f'#{test_case}')
     for i in range(N):
         for j in range(N):
