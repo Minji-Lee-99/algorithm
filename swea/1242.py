cc468cb8d8b2b89cb0f0fc76c2bfeaa13b2196dc
Lee Minji <gagopa9912@naver.com>
2022-09-21 22:08:53 +0900

    0921

--- swea/1242.py
+++ swea/1242.py
@@ -15,8 +15,7 @@ convert_rate = {'211': 0, '221': 1, '122': 2, '411': 3, '132': 4, '231': 5,
 T = int(input())
 for tc in range(1, T + 1):
     N, M = map(int, input().split())   # row, col
-    code = [input() for _ in range(N)]
-    ans = 0
+    code = [input().strip() for _ in range(N)]
 
     # 16진수를 2진수로 변환
     for i in range(N):
@@ -25,98 +24,109 @@ for tc in range(1, T + 1):
             temp += convert[code[i][j]]
         code[i] = temp
 
-    # 끝이 1인 값 찾기
+    # 코드 찾기
+    ans = []
     for i in range(1, N - 1):
-        temp = []
-        for j in range(M * 4 - 2, 0, -1):
-            if code[i][j] == '1' and code[i - 1][j] == '0':
-                y = j
-                rate = []  # 숫자 하나의 코드 비율
+        j = 0
+        while(j < M * 4 - 1):
+            if code[i][j] == '1' and code[i - 1][j] == '0':  # 1이고 바로 위의 값이 0인 경우 찾기
+                rate = []
                 before = '1'
-                cnt = 0
-                while len(rate) < 4 and y > 0:   # y가 인덱스를 벗어나면 멈춤. 그리고 rate가 4일때까지만 진행
-                    if code[i][y] == before:    # 같은 숫자이면 개수 세주기
-                        cnt += 1
-                        y -= 1
-                    else:
-                        if cnt:
-                            rate.append(cnt)
+                for _ in range(3):  # 비율 3자리 찾기(첫번째 0은 필요X)
+                    cnt = 0
+                    while 1:
+                        if code[i][j] == before:
+                            cnt += 1
                         else:
+                            rate.append(cnt)
+                            before = str(abs(int(before) - 1))
                             break
-                        cnt = 0
-                        before = str(abs(int(before) - 1))
-                if k == 7:  # 마지막인 경우에는 0의 개수가 추가되지 않기 때문에 따로 추가해준다.
-                    rate.append(9)
-                if len(rate) == 4:  # 비율의 개수가 4개인 경우
-                    rate = rate[:3]
-                    minV = min(rate)
-                    rate = list(map(lambda x: str(x // minV), rate))
-                    rate = rate[::-1]
-                    rate = ''.join(rate)
-                    if rate in convert_rate:
-                        temp.append(convert_rate[rate])
-                    else:   # 비율값이 존재X
-                        break
-                else:    # rate의 길이가 4가 안되는 경우(y가 인덱스를 벗어난 경우)
-                    break
-            else:    # 중간에 멈추는 것 없이 8개의 숫자를 다 찾았다면 검증
-                even_num = 0
-                for n in range(0, 8, 2):
-                    even_num += temp[n]
-                odd_num = 0
-                for n in range(1, 8, 2):
-                    odd_num += temp[n]
-                if (odd_num * 3 + even_num) % 10 == 0:
-                    ans += sum(temp)
-    print(f'#{tc} {ans}')
+                        j += 1
+                # 비율에 해당하는 10진수 찾기
+                minV = min(rate)
+                rate = list(map(lambda x: str(x // minV), rate))
+                rate = ''.join(rate)
+                if rate in convert_rate:
+                    ans.append(convert_rate[rate])
+            else:
+                j += 1
+
+    # 올바른 암호코드인지 판단하기
+    result = 0
+    for i in range(0, len(ans), 8):  # 8자리씩 끊어서 확인하기
+        even_num = 0
+        for j in range(i+1, i + 8, 2):
+            even_num += ans[j]
+        odd_num = 0
+        for j in range(i, i + 8, 2):
+            odd_num += ans[j]
+        if (odd_num * 3 + even_num) % 10 == 0:
+            result += odd_num
+            result += even_num
+    print(f'#{tc} {result}')
 
 
 
-    # # 끝이 1인 값 찾기
-    # for i in range(1, N - 1):
-    #     for j in range(M * 4 - 2, 0, -1):
-    #         if code[i][j] == '1' and code[i - 1][j] == '0':
-    #             y = j
-    #             temp = []   # 8자리의 숫자를 더하는 변수
-    #             for k in range(8):
-    #                 rate = []  # 숫자 하나의 코드 비율
-    #                 before = '1'
-    #                 cnt = 0
-    #                 while len(rate) < 4 and y > 0:   # y가 인덱스를 벗어나면 멈춤. 그리고 rate가 4일때까지만 진행
-    #                     if code[i][y] == before:    # 같은 숫자이면 개수 세주기
-    #                         cnt += 1
-    #                         y -= 1
-    #                     else:
-    #                         if cnt:
-    #                             rate.append(cnt)
-    #                         else:
-    #                             break
-    #                         cnt = 0
-    #                         before = str(abs(int(before) - 1))
-    #                 if k == 7:  # 마지막인 경우에는 0의 개수가 추가되지 않기 때문에 따로 추가해준다.
-    #                     rate.append(9)
-    #                 if len(rate) == 4:  # 비율의 개수가 4개인 경우
-    #                     rate = rate[:3]
-    #                     minV = min(rate)
-    #                     rate = list(map(lambda x: str(x // minV), rate))
-    #                     rate = rate[::-1]
-    #                     rate = ''.join(rate)
-    #                     if rate in convert_rate:
-    #                         temp.append(convert_rate[rate])
-    #                     else:   # 비율값이 존재X
-    #                         break
-    #                 else:    # rate의 길이가 4가 안되는 경우(y가 인덱스를 벗어난 경우)
-    #                     break
-    #             else:    # 중간에 멈추는 것 없이 8개의 숫자를 다 찾았다면 검증
-    #                 even_num = 0
-    #                 for n in range(0, 8, 2):
-    #                     even_num += temp[n]
-    #                 odd_num = 0
-    #                 for n in range(1, 8, 2):
-    #                     odd_num += temp[n]
-    #                 if (odd_num * 3 + even_num) % 10 == 0:
-    #                     ans += sum(temp)
-    # print(f'#{tc} {ans}')
+# 제일 처음에 짠 코드 비효율적!
+# 제일 뒤에서 보면서 1이 나오면 8글자를 모두 찾도록 해서 한 줄에서 굉장히 많은 비교를 하게 되어 있음.
+# T = int(input())
+# for tc in range(1, T + 1):
+#     N, M = map(int, input().split())  # row, col
+#     code = [input().strip() for _ in range(N)]
+#     ans = 0
+#
+#     # 16진수를 2진수로 변환
+#     for i in range(N):
+#         temp = ''
+#         for j in range(M):
+#             temp += convert[code[i][j]]
+#         code[i] = temp
+#
+#     # 끝이 1인 값 찾기
+#     for i in range(1, N - 1):
+#         for j in range(M * 4 - 2, 0, -1):
+#             if code[i][j] == '1' and code[i - 1][j] == '0':
+#                 y = j
+#                 temp = []   # 8자리의 숫자를 더하는 변수
+#                 for k in range(8):
+#                     rate = []  # 숫자 하나의 코드 비율
+#                     before = '1'
+#                     cnt = 0
+#                     while len(rate) < 4 and y > 0:   # y가 인덱스를 벗어나면 멈춤. 그리고 rate가 4일때까지만 진행
+#                         if code[i][y] == before:    # 같은 숫자이면 개수 세주기
+#                             cnt += 1
+#                             y -= 1
+#                         else:
+#                             if cnt:
+#                                 rate.append(cnt)
+#                             else:
+#                                 break
+#                             cnt = 0
+#                             before = str(abs(int(before) - 1))
+#                     if k == 7:  # 마지막인 경우에는 0의 개수가 추가되지 않기 때문에 따로 추가해준다.
+#                         rate.append(9)
+#                     if len(rate) == 4:  # 비율의 개수가 4개인 경우
+#                         rate = rate[:3]
+#                         minV = min(rate)
+#                         rate = list(map(lambda x: str(x // minV), rate))
+#                         rate = rate[::-1]
+#                         rate = ''.join(rate)
+#                         if rate in convert_rate:
+#                             temp.append(convert_rate[rate])
+#                         else:   # 비율값이 존재X
+#                             break
+#                     else:    # rate의 길이가 4가 안되는 경우(y가 인덱스를 벗어난 경우)
+#                         break
+#                 else:    # 중간에 멈추는 것 없이 8개의 숫자를 다 찾았다면 검증
+#                     even_num = 0
+#                     for n in range(0, 8, 2):
+#                         even_num += temp[n]
+#                     odd_num = 0
+#                     for n in range(1, 8, 2):
+#                         odd_num += temp[n]
+#                     if (odd_num * 3 + even_num) % 10 == 0:
+#                         ans += sum(temp)
+#     print(f'#{tc} {ans}')
 
 
 
--- swea/1244.py
+++ swea/1244.py
@@ -1,28 +1,62 @@
 # 최대 상금
 
-# DFS를 통해서 모든 경우를 확인하는데, 메모이제이션을 통해서 이미 나온 경우는 계산하지 않도록 함.
-def find_max_price(number, k, memo):
-    if k == 0: # base케이스
-        return number
-    if (number, k) in memo: # 메모에 있는 경우 메모값 리턴
-        return memo[(number, k)]
-    for i in range(len(number)): # 모든 경우의 수
-        for j in range(len(number)):
-            if i != j: # i, j가 같지 않을때만 실행
-                number2 = list(number)
-                number2[i], number2[j] = number2[j], number2[i] # 자리 바꾸기
-                num = find_max_price(''.join(number2), k-1, memo) # 자리 바꾼 상태에서 다시 자리바꾸기(k번 만큼)
-                if (number, k) in memo: # 메모에 적어주기
-                    if memo[(number, k)] < num: # 이미 메모에 있다면 새로운 값이 더 큰 경우만 바꾸기
-                        memo[(number, k)] = num
-                else:
-                    memo[(number, k)] = num
-    return memo[(number, k)] # 메모에서 최종값 리턴
+def find_max_price(num, k, d):
+    global max_num
+    if d == k:   # 다 바꾸었다면 최댓값 갱신
+        r = int(''.join(num))
+        if r > max_num:
+            max_num = r
+        return
+    if ''.join(num) in memo[d]:   # 이미 본 경우라면 가지치기
+        return
+    memo[d].append(''.join(num))    # 메모에 남겨주기
+    # 바꿀 횟수가 남았다면 바꾸기!
+    for i in range(0, N - 1):
+        for j in range(i + 1, N):
+            num[i], num[j] = num[j], num[i]
+            find_max_price(num, k, d + 1)
+            num[i], num[j] = num[j], num[i]
 
 
 T = int(input())
 for tc in range(1, T + 1):
     num, k = input().split()
+    N = len(num)
     k = int(k)
-    result = find_max_price(num, k, {})
-    print(f'#{tc} {result}')
+    num = list(num)
+    memo = [[] for _ in range(k)]
+    max_num = 0
+    find_max_price(num, k, 0)
+    print(f'#{tc} {max_num}')
+
+
+
+
+
+
+# # DFS를 통해서 모든 경우를 확인하는데, 메모이제이션을 통해서 이미 나온 경우는 계산하지 않도록 함.
+# def find_max_price(number, k, memo):
+#     if k == 0: # base케이스
+#         return number
+#     if (number, k) in memo: # 메모에 있는 경우 메모값 리턴
+#         return memo[(number, k)]
+#     for i in range(len(number)): # 모든 경우의 수
+#         for j in range(len(number)):
+#             if i != j: # i, j가 같지 않을때만 실행
+#                 number2 = list(number)
+#                 number2[i], number2[j] = number2[j], number2[i] # 자리 바꾸기
+#                 num = find_max_price(''.join(number2), k-1, memo) # 자리 바꾼 상태에서 다시 자리바꾸기(k번 만큼)
+#                 if (number, k) in memo: # 메모에 적어주기
+#                     if memo[(number, k)] < num: # 이미 메모에 있다면 새로운 값이 더 큰 경우만 바꾸기
+#                         memo[(number, k)] = num
+#                 else:
+#                     memo[(number, k)] = num
+#     return memo[(number, k)] # 메모에서 최종값 리턴
+#
+#
+# T = int(input())
+# for tc in range(1, T + 1):
+#     num, k = input().split()
+#     k = int(k)
+#     result = find_max_price(num, k, {})
+#     print(f'#{tc} {result}')
