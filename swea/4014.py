6e0ad60b201b53021e6b42010ff5772b9cd1bcf4
Lee Minji <gagopa9912@naver.com>
2022-10-27 20:44:14 +0900

    1027

--- swea/4014.py
+++ swea/4014.py
@@ -6,7 +6,7 @@ for tc in range(1, T + 1):
     ans = 0
     # 가로 확인하기
     for i in range(N):
-        num = []
+        num = []  # 예를 들어서  3 3 3 2 1 1로 지형이 주어졌다면 num = [3, 2, 1] cnt = [3, 1, 2]
         cnt = []
         now = arr[i][0]
         c = 0
@@ -21,20 +21,20 @@ for tc in range(1, T + 1):
         num.append(now)
         cnt.append(c)
         for idx in range(len(num) - 1):
-            if -1 <= num[idx] - num[idx + 1] <= 1:
-                if num[idx] > num[idx + 1]:
-                    if cnt[idx + 1] >= K:
-                        cnt[idx + 1] -= K
-                    else:
+            if -1 <= num[idx] - num[idx + 1] <= 1:  # 높이 차이가 1인 경우만 실행
+                if num[idx] > num[idx + 1]:  # 경사로는 더 작은 쪽에 설치
+                    if cnt[idx + 1] >= K:  # 경사로의 가로 길이보다 개수 더 많거나 같아야 설치 가능
+                        cnt[idx + 1] -= K   # 이미 설치한 곳에는 또 설치가 불가능 하기 때문에 개수 빼주기
+                    else:  # 설치할 공간이 없는 경우
                         break
                 else:
                     if cnt[idx] >= K:
                         cnt[idx] -= K
-                    else:
+                    else:  # 설치할 공간이 없는 경우
                         break
-            else:
+            else:  # 높이 차이가 1보다 크면 멈추기
                 break
-        else:
+        else:  # break가 없는 경우에만 카운트
             ans += 1
 
     # 세로 확인하기
--- swea/5653.py
+++ swea/5653.py
@@ -5,42 +5,42 @@ from heapq import heappop, heappush
 
 T = int(input())
 for tc in range(1, T + 1):
-    H, W, K = map(int, input().split())
+    H, W, K = map(int, input().split())  # 행, 열, 시간
     arr = [list(map(int, input().split())) for _ in range(H)]
     visited = set()
     inactive = []
     active = []
-    sub = []
     # 세포의 위치 추출
     for i in range(H):
         for j in range(W):
             if arr[i][j]:
                 visited.add((i, j))
-                heappush(inactive, (arr[i][j], arr[i][j] * -1, i, j))
+                heappush(inactive, (arr[i][j], arr[i][j] * -1, i, j))  # 세포가 활성화되는 시간, 생명력, 좌표(생명력에 -1을 해주는 이유는 같은 시간에 활성화되는 세포들 중에서 생명력이 높은 것만 가지고 오기 위함)
     del arr
     t = 1
     while t <= K:
-        # t초에 활성화 되는 세포들 옮기기
+        # t초에 활성화 되는 세포들 active로 옮기고, 분열해주기
         while inactive:
             temp = heappop(inactive)
             if temp[0] == t:  # 활성화 시키기
-                heappush(active, t + temp[1] * -1)
-                if t + 1 <= K:
+                heappush(active, t + temp[1] * -1)  # 활성화 큐에 죽는 시간을 계산해서 넣어줍니다.
+                if t + 1 <= K:  # 분열되는 것은 주어진 시간이 현재보다 1이 더 많을 때만 가능
                     # 4방향 탐색
                     for di, dj in [[1, 0], [0, 1], [-1, 0], [0, -1]]:
                         ni, nj = temp[2] + di, temp[3] + dj
-                        if not (ni, nj) in visited:
+                        if not (ni, nj) in visited:  # 해당 좌표에 세포가 없을 때만 실행
                             visited.add((ni, nj))
                             heappush(inactive, (t + (temp[1] * -1) + 1, temp[1], ni, nj))
             else:
-                heappush(inactive, temp)
+                heappush(inactive, temp)  # 빼서 확인해봤는데 아직 활성화될 시간이 아니라면 멈추기
                 break
+        # t초에 죽는 세포들 찾아서 없애주기
         while active:
             temp2 = heappop(active)
             if temp2 != t:
                 heappush(active, temp2)
                 break
-        t += 1
+        t += 1  # 시간 갱신
     print(f'#{tc} {len(active) + len(inactive)}')
 
 
