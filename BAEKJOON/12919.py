34ab78cc800bbc6fd3c0237e0ad5e6fa31cdbfcb
LeeMJ <gagopa9912@naver.com>
2023-01-12 12:06:20 +0900

    0112

--- BAEKJOON/12919.py
+++ BAEKJOON/12919.py
@@ -1,28 +1,27 @@
 # A와 B 2
-def bf(cnt_a, cnt_b, word, word_length):
+#
+
+def bf(T):
     global flag
-    if word == T:
-        flag = True
-        return
-    elif cnt_a > target_cnt_a or cnt_b > target_cnt_b:
-        return
-    elif word_length >= target_length:
-        return
     if flag:
         return
-    bf(cnt_a + 1, cnt_b, word + 'A', word_length + 1)
-    bf(cnt_a, cnt_b + 1, 'B' + word[::-1], word_length + 1)
+    if S == T:
+        flag = True
+        return
+    if len(T) == 0:
+        return
+    if T[-1] == 'A':
+        bf(T[:-1])
+    if T[0] == 'B':
+        bf(T[1:][::-1])
     return
 
 
 S = input()
 T = input()
 
-target_length = len(T)
-target_cnt_a = T.count('A')
-target_cnt_b = T.count('B')
 
 flag = False
-bf(S.count('A'), S.count('B'), S, len(S))
+bf(T)
 print(int(flag))
 
